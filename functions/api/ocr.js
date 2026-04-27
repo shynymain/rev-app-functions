38b239507e31b9ba6ffbb8821fe9c64c3be75cd2
shynymain <umeparis0317@gmail.com>
2026-04-28 05:05:26 +0900

    Update ocr.js

--- functions/api/ocr.js
+++ functions/api/ocr.js
@@ -1,4 +1,8 @@
 const response = await env.AI.run("@cf/meta/llama-3.2-11b-vision-instruct", {
   image: bytes,
-  prompt: "agree\n" + prompt
+  prompt,
+  raw: true,
+  extraHeaders: {
+    "x-cf-ai-accept-terms": "true"
+  }
 });
